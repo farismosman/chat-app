fdf30075730ea97524cac5a07cf274c93e6639bd
Faris Mohammed <farismosman@gmail.com>
2017-11-20 22:47:06 +0200

    adds basic migrations and a user model

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='timesmedia',
-    packages=['src'],
+    packages=['src', 'src.models'],
     include_package_data=True,
     install_requires=[
         'flask',
         'flask-sqlalchemy',
         'alembic',
-        'psycopg2'
+        'psycopg2',
+        'flask-migrate',
+        'flask-script'
     ],
 )
--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+db = SQLAlchemy()
+
+POSTGRES = {
+    'user': 'chatter',
+    'pw': 'Mxwp78RB4rcjKa',
+    'db': 'timesmedia',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 import src.routes
+from models.user import User
