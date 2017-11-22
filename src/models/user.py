ec94c862def84b49a8111dccc548425312915291
Faris Mohammed <farismosman@gmail.com>
2017-11-22 21:46:47 +0200

    added unique constraints to username

--- src/models/user.py
+++ src/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(), nullable=False)
     last_name = db.Column(db.String(), nullable=False)
-    username = db.Column(db.String(), nullable=False)
+    username = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
